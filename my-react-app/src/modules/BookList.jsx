bdbb8026e70136ea82b543cf1167c8bcc9c7e0a8
2400080241 <2400031862@kluniversity.in>
2025-10-30 09:52:59 +0530

    my-react-app

---
+++ my-react-app/src/modules/BookList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export function BookList({ books, onAddToCart }) {
+  return (
+    <div>
+      <h2>Available Books</h2>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>
+            <span>
+              {book.title} by {book.author} – ₹{book.price} ({book.availability})
+            </span>
+            <button
+              onClick={() => onAddToCart(book)}
+              disabled={book.availability !== 'in stock'}
+            >
+              Add to Cart
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
